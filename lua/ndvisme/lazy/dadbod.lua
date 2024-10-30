d8dd4ae7d4fa9c8fa7c53773ea74573af002d29d
nadav-sgh <nspier@sightd.io>
2024-10-30 13:46:23 +0200

    Add auto complete suggestions for dadbod.

--- lua/ndvisme/lazy/dadbod.lua
+++ lua/ndvisme/lazy/dadbod.lua
@@ -1,4 +1,24 @@
 return {
-  'tpope/vim-dadbod',
-  'kristijanhusak/vim-dadbod-ui'
+    {
+        'tpope/vim-dadbod',
+        dependencies = {
+            'kristijanhusak/vim-dadbod-ui',
+            'kristijanhusak/vim-dadbod-completion',
+        },
+        config = function()
+            vim.g.db_ui_use_nerd_fonts = 1
+            
+            -- Add autocmd for SQL files
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = {
+                    "sql",
+                    "mysql",
+                    "plsql",
+                },
+                callback = function()
+                    vim.opt_local.omnifunc = "vim_dadbod_completion#omni"
+                end
+            })
+        end,
+    }
 }
--- lua/ndvisme/lazy/lsp.lua
+++ lua/ndvisme/lazy/lsp.lua
@@ -123,6 +123,7 @@ return {
 			sources = cmp.config.sources({
 				{ name = 'nvim_lsp' },
 				{ name = 'luasnip' }, -- For luasnip users.
+				 { name = 'vim-dadbod-completion' },
 			}, {
 					{ name = 'buffer' },
 				})
