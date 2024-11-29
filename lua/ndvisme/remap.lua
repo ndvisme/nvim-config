c80a9fe91fbe6c0a2526f0962e172862120eb6ec
nadav-sgh <nspier@sightd.io>
2024-11-29 12:42:50 +0200

    Add documantaion for keymapping.

--- lua/ndvisme/remap.lua
+++ lua/ndvisme/remap.lua
@@ -12,6 +12,7 @@
 -- <nop> --> No operation
 -- buffer --> tmp space in memory that holds the content of an opened file for editing
 -- quickfix list --> Compile errs, linter errs, search results & more...
+-- % --> Represents current file path
 
 vim.g.mapleader = " "
 
@@ -76,17 +77,22 @@ end)
 -- Navigate to the next/prev item in the quickfix list while keeping the cursor in the middle of the screen
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-
-vim.keymap.set('n', 'K', vim.lsp.buf.hover)
-
+-- Similar to the above cmd but specific to the current window
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
+vim.keymap.set('n', 'K', vim.lsp.buf.hover)
+
+-- Searches in current file by regex
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Make current file executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
+-- "make it rain" animation
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
+-- Source (reload) the current file
 vim.keymap.set("n", "<leader><leader>", function()
 	vim.cmd("so")
 end)
