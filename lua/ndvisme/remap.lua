28f96aa3ee8947c6e5025210385985cef029a79a
nadav-sgh <nspier@sightd.io>
2024-11-29 11:43:23 +0200

    Add docomentaion to keymaps.

--- lua/ndvisme/remap.lua
+++ lua/ndvisme/remap.lua
@@ -1,16 +1,34 @@
+-- CR --> carriage return == the enter action
+-- '> --> Represents the end of the visual selection
+-- '< --> represents the start of the visual selection
+-- = --> Reindents the line
+-- m --> Mark. Act like bookmarks that allow u to save specific positions in txt & jump back to them later
+--		` --> Jumps to the exact line & column position of the mark
+--		' --> Jumps to the beggining of the line containing the mark
+-- <C-x> --> ctrl + x
 
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>pb", "<CMD>Telescope buffers<CR>")
 
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- Project View
+vim.keymap.set("n", "<leader>pb", "<CMD>Telescope buffers<CR>") -- Project Buffer
+
+-- This moves the selected lines in visual mode up or down a line
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- In normal mode join lines while keeping the the cursor in its original position
+-- cmd operations --> mark & save in z. Join lines. jump to exact mark position.
 vim.keymap.set("n", "J", "mzJ`z")
+
+-- In normal mode goes down half a page & than position the cursor in the middle of the screen
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- When searching for a pattern in normal mode, this goes to the next occurnce.
+-- cmd operation --> next pattern match. cursor in middle of screen. open any folds in the cursor position
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
 vim.keymap.set("n", "<leader>zig", "<cmd>LspRestart<cr>")
 
 vim.keymap.set("n", "<leader>vwm", function()
