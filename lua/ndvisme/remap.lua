8c102f07d212e6f2b08431991460ab2ebed9c436
nadav-sgh <nspier@sightd.io>
2024-12-11 11:56:27 +0200

    add tmux nvim navigator plugin.

--- lua/ndvisme/remap.lua
+++ lua/ndvisme/remap.lua
@@ -96,3 +96,9 @@ vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 vim.keymap.set("n", "<leader><leader>", function()
 	vim.cmd("so")
 end)
+
+-- navigate panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
