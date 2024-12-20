32d25f0c5f3cd3f03532bcb99b614efdde9fab87
nadav-sgh <nspier@sightd.io>
2024-12-20 13:04:13 +0200

    add source cmnds to init & highlight on yank.

---
+++ init.lua
@@ -0,0 +1,15 @@
+print("hello from init")
+
+
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
+
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "high light when yanking text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function ()
+		vim.highlight.on_yank()
+	end,
+})
