6266cde2b7dfcbb889c2dfd25bd979c00e67bf7b
nadav-sgh <nspier@sightd.io>
2024-12-20 13:11:28 +0200

    add desc next to source keymaps.

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 print("hello from init")
 
 
+-- These keymaps make development much more quicker
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
