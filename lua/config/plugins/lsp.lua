e8cf51bdc7f21575be838f22c6beadff1383146c
nadav-sgh <nspier@sightd.io>
2024-12-21 02:39:34 +0200

    revert to before java.

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -4,18 +4,20 @@ return {
     "neovim/nvim-lspconfig",
     dependencies = {
       {
-        "folke/lazydev.nvim",
-        ft = "lua",
-        opts = {
-          library = {
-            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-          },
-        },
+	"folke/lazydev.nvim",
+	ft = "lua", -- only load on lua files
+	opts = {
+	  library = {
+	    -- See the configuration section for more details
+	    -- Load luvit types when the `vim.uv` word is found
+	    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+	  },
+	},
       },
     },
     config = function()
-      -- Lua LSP setup
       require("lspconfig").lua_ls.setup {}
+
     end,
   }
 }
