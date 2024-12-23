916ae526cc22cfa1f43b31ba84c7086622f60405
Christian Valenzuela <chris.a.valenzuela@protonmail.com>
2024-12-22 20:06:03 -0800

    add on_attach handler

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,5 +1,7 @@
+local on_attach = require("config.on_attach")
 local config = {
     cmd = {vim.fn.expand('~/.local/share/nvim/mason/bin/jdtls')},
     root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
+    on_attach = on_attach,
 }
 require('jdtls').start_or_attach(config)
