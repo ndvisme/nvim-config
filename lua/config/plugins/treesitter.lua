fbd4ef4b68747c9496dbdb3ef42c709b00531bcc
Christian Valenzuela <chris.a.valenzuela@protonmail.com>
2024-12-22 18:19:54 -0800

    reformat

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -1,31 +1,35 @@
 -- A must have parser
 return {
   {
-    "nvim-treesitter/nvim-treesitter", 
+    "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
       require'nvim-treesitter.configs'.setup {
-	-- A list of parser names, or "all" (the listed parsers MUST always be installed)
-	ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "java"},
+        -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "java"},
 
+        -- Automatically install missing parsers when entering buffer
+        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+        auto_install = true,
 
-	-- Automatically install missing parsers when entering buffer
-	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-	auto_install = true,
+        highlight = {
+          enable = true,
+          disable = function(lang, buf)
+              local max_filesize = 100 * 1024 -- 100 KB
+              local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+              if ok and stats and stats.size > max_filesize then
+                return true
+              end
+          end,
+          additional_vim_regex_highlighting = false,
+        },
 
-	highlight = {
-	  enable = true,
+        -- defaults
+        syn_install = false,
 
-	  disable = function(lang, buf)
-	      local max_filesize = 100 * 1024 -- 100 KB
-	      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-	      if ok and stats and stats.size > max_filesize then
-		  return true
-	      end
-	  end,
-	  additional_vim_regex_highlighting = false,
-	},
       }
-      end
-  }
+
+    end
+  },
+
 }
