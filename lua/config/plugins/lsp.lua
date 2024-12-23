624525cdbaa9d9f2af20a15abcc0bb7188454e01
nadav-sgh <nspier@sightd.io>
2024-12-23 22:40:24 +0200

    add 2 indent for py, java, lua, sh.

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -1,30 +1,29 @@
-
 return {
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      {
-        "folke/lazydev.nvim",
-        ft = "lua",
-        opts = {
-          library = {
-            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-          },
-        },
-      },
-    },
-    config = function()
-      require("lspconfig").lua_ls.setup {}
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{
+				"folke/lazydev.nvim",
+				ft = "lua",
+				opts = {
+					library = {
+						{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+					},
+				},
+			},
+		},
+		config = function()
+			require("lspconfig").lua_ls.setup {}
 
-      vim.api.nvim_create_autocmd('LspAttach', {
-        callback = function(args)
-          local client = vim.lsp.get_client_by_id(args.data.client_id)
-          if not client then return end
+			vim.api.nvim_create_autocmd('LspAttach', {
+				callback = function(args)
+					local client = vim.lsp.get_client_by_id(args.data.client_id)
+					if not client then return end
 
-          vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format() end)
-          vim.keymap.set('n', '<space>gD', function() vim.lsp.buf.declaration() end)
-        end,
-      })
-    end,
-  }
+					vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format() end)
+					vim.keymap.set('n', '<space>gD', function() vim.lsp.buf.declaration() end)
+				end,
+			})
+		end,
+	}
 }
