242b468159fa9817df338c001791b730ec57b412
nadav-sgh <nspier@sightd.io>
2024-12-23 22:30:47 +0200

    add go to declaration inside the LspAttach event auto command.

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -1,26 +1,30 @@
--- for thoese basic default configs for the nvim lsp client for various lsp servers
+
 return {
   {
     "neovim/nvim-lspconfig",
     dependencies = {
       {
-	"folke/lazydev.nvim",
-	ft = "lua", -- only load on lua files
-	opts = {
-	  library = {
-	    -- See the configuration section for more details
-	    -- Load luvit types when the `vim.uv` word is found
-	    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-	  },
-	},
+        "folke/lazydev.nvim",
+        ft = "lua",
+        opts = {
+          library = {
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
+        },
       },
     },
     config = function()
-	require("lspconfig").lua_ls.setup {}
+      require("lspconfig").lua_ls.setup {}
 
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if not client then return end
 
-      vim.keymap.set('n', '<space>gD', function() vim.lsp.buf.declaration() end)
-      vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format() end)
+          vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format() end)
+          vim.keymap.set('n', '<space>gD', function() vim.lsp.buf.declaration() end)
+        end,
+      })
     end,
   }
 }
