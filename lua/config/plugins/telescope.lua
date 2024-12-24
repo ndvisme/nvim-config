ff2aa4791a1c0feb9814366a23bceefbbff6669b
nadav-sgh <nspier@sightd.io>
2024-12-25 00:31:12 +0200

    add live grep keymapping (fg) with ivy theme.

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -11,20 +11,18 @@ return {
     config = function()
         require('telescope').setup {
             pickers = {
-                find_files = {
-                    theme = "ivy",
-                },
-                help_tags = {
-                    theme = "ivy",
-                }
+                find_files = { theme = "ivy",},
+                help_tags = { theme = "ivy",},
+                live_grep = { theme = "ivy",},
             }
         }
 
         pcall(require('telescope').load_extension, 'fzf') -- this makes the search faster!
 
-        -- Your keymaps remain the same
         vim.keymap.set('n', '<space>fh', require('telescope.builtin').help_tags)
         vim.keymap.set('n', '<space>fd', require('telescope.builtin').find_files)
+        vim.keymap.set('n', '<space>fg', require('telescope.builtin').live_grep)
+
         vim.keymap.set('n', '<space>en', function()
             require('telescope.builtin').find_files {
                 cwd = vim.fn.stdpath('config')
