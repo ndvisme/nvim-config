ef1f3aa7c1d68ac2e563ebbb233d50b100e23cf5
nadav-sgh <nspier@sightd.io>
2024-12-24 19:14:45 +0200

    add basic config to navigate files via telescope.

---
+++ lua/config/plugins/telescope.lua
@@ -0,0 +1,13 @@
+-- for navigation the file systems
+return { {
+	'nvim-telescope/telescope.nvim',
+	tag = '0.1.8',
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+	},
+	config = function()
+		print("telescope")
+	end,
+}
+}
