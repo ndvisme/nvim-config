408c9c133c31736ea0eae6eed96b9ffe889b4a16
nadav-sgh <nspier@sightd.io>
2024-12-25 00:01:11 +0200

    add find help tags to get help for different nvim commands.

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -7,6 +7,17 @@ return { {
 		{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
 	},
 	config = function()
+		require('telescope').setup {
+			pickers = {
+				find_files = {
+					theme = "ivy", -- this shows more data when looking for files
+				},
+				help_tags = {
+					theme = "ivy", -- this shows more data when looking for files
+				}
+			}
+		}
+		vim.keymap.set('n', '<space>fh', require('telescope.builtin').help_tags)
 		vim.keymap.set('n', '<space>fd', require('telescope.builtin').find_files)
 		vim.keymap.set('n', '<space>en', function()
 			require('telescope.builtin').find_files {
