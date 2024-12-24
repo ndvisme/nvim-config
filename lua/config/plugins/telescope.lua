a9ab3e1787a89cadf4579a5c8b63ec30cde43751
nadav-sgh <nspier@sightd.io>
2024-12-25 00:20:48 +0200

    add fzf plugin as dependency to telescope for faster search.

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -1,29 +1,34 @@
--- for navigation the file systems
-return { {
-	'nvim-telescope/telescope.nvim',
-	tag = '0.1.8',
-	dependencies = {
-		'nvim-lua/plenary.nvim',
-		{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-	},
-	config = function()
-		require('telescope').setup {
-			pickers = {
-				find_files = {
-					theme = "ivy", -- this shows more data when looking for files
-				},
-				help_tags = {
-					theme = "ivy", -- this shows more data when looking for files
-				}
-			}
-		}
-		vim.keymap.set('n', '<space>fh', require('telescope.builtin').help_tags)
-		vim.keymap.set('n', '<space>fd', require('telescope.builtin').find_files)
-		vim.keymap.set('n', '<space>en', function()
-			require('telescope.builtin').find_files {
-				cwd = vim.fn.stdpath('config')
-			}
-		end, { desc = 'its conviniet to be able to edit config where ever and whenever' })
-	end,
-}
+return {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        {
+            'nvim-telescope/telescope-fzf-native.nvim',
+            build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release'
+        }
+    },
+    config = function()
+        require('telescope').setup {
+            pickers = {
+                find_files = {
+                    theme = "ivy",
+                },
+                help_tags = {
+                    theme = "ivy",
+                }
+            }
+        }
+
+        pcall(require('telescope').load_extension, 'fzf') -- this makes the search faster!
+
+        -- Your keymaps remain the same
+        vim.keymap.set('n', '<space>fh', require('telescope.builtin').help_tags)
+        vim.keymap.set('n', '<space>fd', require('telescope.builtin').find_files)
+        vim.keymap.set('n', '<space>en', function()
+            require('telescope.builtin').find_files {
+                cwd = vim.fn.stdpath('config')
+            }
+        end, { desc = 'its conviniet to be able to edit config where ever and whenever' })
+    end
 }
