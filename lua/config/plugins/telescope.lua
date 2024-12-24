a8fe1315120c1a9d230a137b2c0b481b252eb359
nadav-sgh <nspier@sightd.io>
2024-12-25 00:48:15 +0200

    add telescope keymap for man pages.

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -1,32 +1,34 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        {
-            'nvim-telescope/telescope-fzf-native.nvim',
-            build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release'
-        }
-    },
-    config = function()
-        require('telescope').setup {
-            pickers = {
-                find_files = { theme = "ivy",},
-                help_tags = { theme = "ivy",},
-                live_grep = { theme = "ivy",},
-            }
-        }
+	'nvim-telescope/telescope.nvim',
+	tag = '0.1.8',
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		{
+			'nvim-telescope/telescope-fzf-native.nvim',
+			build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release'
+		}
+	},
+	config = function()
+		require('telescope').setup {
+			pickers = {
+				find_files = { theme = "ivy", },
+				help_tags = { theme = "ivy", },
+				live_grep = { theme = "ivy", },
+			}
+		}
 
-        pcall(require('telescope').load_extension, 'fzf') -- this makes the search faster!
+		pcall(require('telescope').load_extension, 'fzf') -- this makes the search faster!
 
-        vim.keymap.set('n', '<space>fh', require('telescope.builtin').help_tags)
-        vim.keymap.set('n', '<space>fd', require('telescope.builtin').find_files)
-        vim.keymap.set('n', '<space>fg', require('telescope.builtin').live_grep)
+		vim.keymap.set('n', '<space>fh', require('telescope.builtin').help_tags, { desc = 'get nvim cmds help' })
+		vim.keymap.set('n', '<space>fm', require('telescope.builtin').man_pages,
+			{ desc = 'see help for different cmnds of apps' })
+		vim.keymap.set('n', '<space>fd', require('telescope.builtin').find_files)
+		vim.keymap.set('n', '<space>fg', require('telescope.builtin').live_grep)
 
-        vim.keymap.set('n', '<space>en', function()
-            require('telescope.builtin').find_files {
-                cwd = vim.fn.stdpath('config')
-            }
-        end, { desc = 'its conviniet to be able to edit config where ever and whenever' })
-    end
+		vim.keymap.set('n', '<space>en', function()
+			require('telescope.builtin').find_files {
+				cwd = vim.fn.stdpath('config')
+			}
+		end, { desc = 'its conviniet to be able to edit config where ever and whenever' })
+	end
 }
