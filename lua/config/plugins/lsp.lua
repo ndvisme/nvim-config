7a5796718db6697a846c0a9d74457bce395b5f9e
nadav-sgh <nspier@sightd.io>
2024-12-25 22:06:44 +0200

    use blink instead of nvim-cmp for auto completions.

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -2,6 +2,7 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		dependencies = {
+			'saghen/blink.cmp',
 			{
 				"folke/lazydev.nvim",
 				ft = "lua",
@@ -13,7 +14,8 @@ return {
 			},
 		},
 		config = function()
-			require("lspconfig").lua_ls.setup {}
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
+      require("lspconfig").lua_ls.setup { capabilites = capabilities }
 
 			vim.api.nvim_create_autocmd('LspAttach', {
 				callback = function(args)
