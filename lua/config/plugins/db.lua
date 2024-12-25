2bbbe37caf3da8e295d857d8ff2ca20be6cb5506
nadav-sgh <nspier@sightd.io>
2024-12-25 22:38:47 +0200

    connect completion to dadbot (db) plugins.

--- lua/config/plugins/db.lua
+++ lua/config/plugins/db.lua
@@ -1,6 +1,29 @@
 return {
-	"tpope/vim-dadbod",
-	"kristijanhusak/vim-dadbod-completion",
-	"kristijanhusak/vim-dadbod-ui",
+	{
+		'kristijanhusak/vim-dadbod-ui',
+		dependencies = {
+			{ 'tpope/vim-dadbod',                     lazy = true },
+			{ 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+		},
+		cmd = {
+			'DBUI',
+			'DBUIToggle',
+			'DBUIAddConnection',
+			'DBUIFindBuffer',
+		},
+		init = function()
+			vim.g.db_ui_use_nerd_fonts = 1
+		end,
+	},
+	{
+		'saghen/blink.cmp',
+		opts = {
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer", "dadbod" },
+				providers = {
+					dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+				},
+			},
+		},
+	}
 }
-
