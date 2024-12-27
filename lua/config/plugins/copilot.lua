43ea31aca8d133b769a03fe63e4f5788bc24c4f6
nadav-sgh <nspier@sightd.io>
2024-12-27 04:38:12 +0200

    Add disable copilot when nvim starts.

--- lua/config/plugins/copilot.lua
+++ lua/config/plugins/copilot.lua
@@ -4,9 +4,13 @@ return {
 		cmd = "Copilot",
 		lazy = true,
 		config = function()
-			-- Disable default mapping
 			vim.g.copilot_no_tab_map = true
 			vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+
+			vim.api.nvim_create_autocmd("VimEnter", {
+				desc = "I want help only when I invoke it",
+				command = "Copilot disable",
+			})
 		end,
 	}
 }
