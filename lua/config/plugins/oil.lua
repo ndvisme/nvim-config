ba74130a294559716356453a362b7c2cb29ed6d7
nadav-sgh <nspier@sightd.io>
2024-12-28 18:00:10 +0200

    Add show 'hidden files' in oil tree.

--- lua/config/plugins/oil.lua
+++ lua/config/plugins/oil.lua
@@ -3,7 +3,11 @@ return {
 		'stevearc/oil.nvim',
 		---@module 'oil'
 		---@type oil.SetupOpts
-		opts = {},
+		opts = {
+			view_options = {
+				show_hidden = true,
+			},
+		},
 		dependencies = { { "echasnovski/mini.icons", opts = {} } },
 		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 	}
