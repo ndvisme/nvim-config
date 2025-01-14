2d50a80dc210ea8cffbc62b123378993ca79fb4f
nadav-sgh <nspier@sightd.io>
2025-01-14 13:45:11 +0200

    Add sql formatter.

--- lua/config/plugins/conform.lua
+++ lua/config/plugins/conform.lua
@@ -1,12 +1,14 @@
+-- To make my code nice & formatted
 return {
 	{
-		'stevearc/conform.nvim',
+		"stevearc/conform.nvim",
 		opts = {
 			formatters_by_ft = {
 				java = { "google-java-format" },
 				python = { "black" },
 				lua = { "stylua" },
-			}
+				sql = { "pg_format" },
+			},
 		},
-	}
+	},
 }
