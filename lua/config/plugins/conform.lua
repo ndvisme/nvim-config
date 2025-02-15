34d83e9836e7903a3765ff4e3187f249215cacb5
nadav-sgh <nspier@sightd.io>
2025-02-15 13:52:34 +0200

    Update coform to format only in given space.

--- lua/config/plugins/conform.lua
+++ lua/config/plugins/conform.lua
@@ -1,14 +1,26 @@
--- To make my code nice & formatted
 return {
-	{
-		"stevearc/conform.nvim",
-		opts = {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
 			formatters_by_ft = {
 				java = { "google-java-format" },
-				python = { "black" },
-				lua = { "stylua" },
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				svelte = { "prettier" },
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
 				sql = { "pg_format" },
-				json = { "jq" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				graphql = { "prettier" },
+				lua = { "stylua" },
+				python = { "isort", "black" },
 			},
 			formatters = {
 				jq = {
@@ -18,6 +30,14 @@ return {
 					end,
 				},
 			},
-		},
-	},
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
 }
