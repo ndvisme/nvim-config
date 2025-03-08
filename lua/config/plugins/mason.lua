abeddcbe583c9106dc02e1e874f54ade8ad0bfcb
ndvisme <herespier@yahoo.com>
2025-03-08 13:43:10 +0200

    Return mason with required packages.

---
+++ lua/config/plugins/mason.lua
@@ -0,0 +1,18 @@
+return {
+	"williamboman/mason.nvim",
+	opts = {
+		ensure_installed = {
+			"jdtls",
+			"pyright",
+			"black",
+			"debugpy",
+			"flake8",
+			"google-java-format",
+			"isort",
+			"jq",
+			"prettier",
+			"stylelua",
+			"lua_ls",
+		},
+	},
+}
