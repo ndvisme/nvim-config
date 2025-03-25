75e60221a4cea739f5fcced3eb6eabdea9b0f1a0
ndvisme <herespier@yahoo.com>
2025-03-25 06:49:48 +0200

    Add rebase when pull to future tips.

--- nvim_plugins/plugins/present.nvim/lua/present.lua
+++ nvim_plugins/plugins/present.nvim/lua/present.lua
@@ -9,6 +9,7 @@ print("Tips Of The Week: `git reset HEAD~X`, Folding scopes")
 - Git:
 	- git commit --amend (apend current staged changes to last commit)
 	- git reset HEAD~X
+	- git pull -r (rebase)
 
 - Done:
   - Macros
